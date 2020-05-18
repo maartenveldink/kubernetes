cee4dddf72725e8ec25e296db324be587e280f53
Maarten Veldink <maarten.veldink@kadaster.nl>
2020-05-18 14:11:11 +0200

    To document what has been done and keep it repeatable, created a shell script

---
+++ minikube/startup.sh
@@ -0,0 +1,24 @@
+echo This script assumes that you have minikube installed on your path
+
+echo Deleting proxy settings:
+env | grep proxy
+unset http_proxy
+unset https_proxy
+unset no_proxy
+echo Proxy settings deleted:
+env | grep proxy
+
+
+echo Starting minikube...
+minikube start --driver=hyperv
+
+
+echo Set alias kubectl to use minikube internal kubectl version
+alias kubectl='minikube kubectl --'
+echo Verifying:
+kubectl get nodes
+
+mvn clean install -DskipTests
+docker build -t tetris:latest .
+
+alias tetris='minikube service tetris-service --url'
